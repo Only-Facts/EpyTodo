c1af02af35571d57442a80ca14cebe7784957d33
raxfacts <raphael.farge@epitech.eu>
2025-05-16 12:00:23 +0200

    [DELETE] /register & /login from main javascript file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const auth = require('./routes/auth/');
 const user = require('./routes/user');
 const users = require('./routes/users');
 const todos = require('./routes/todos');
@@ -10,39 +11,18 @@ require('dotenv').config();
 const IP = process.env.MYSQL_HOST
 const PORT = process.env.PORT || 8080;
 
-const { addUser } = require('./config/db.mjs');
-
 app.get('/', (_, res) => {
   res.status(200).send({
     message: 'Welcome to EpyTodo',
   });
 });
 
-app.post('/register', async (req, res) => {
-  const { email, name, firstname, password } = req.body;
-  try {
-    await addUser(email, password, name, firstname)
-    res.status(201).send({ "token": "Token of the newly registered user" })
-  } catch (error) {
-    console.error(`${error}`);
-    if (error == `Error: Duplicate entry '${email}' for key 'email'`)
-      res.status(200).send({ "msg": "Account already exists" });
-    else
-      res.status(400).json({ "msg": "Bad parameter" })
-  }
-});
-
-app.post('/login', (_, res) => {
-  res.status(201).json({
-    message: 'connect a user',
-  });
-});
-
+app.use('/', auth);
 app.use('/user', user);
 app.use('/users', users);
 app.use('/todos', todos);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, _) => {
   console.error(err.stack);
   res.status(500).json({ "msg": "Internal server error" });
 })
