b40f61e16b2b880ce8113e199b90740d3fdd50f6
raxfacts <raphael.farge@epitech.eu>
2025-05-16 11:58:24 +0200

    [ADD] User | Token verification in user

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth.js');
 
 const { getUsers, getTodos } = require('../../config/db.mjs');
 
-router.get('/', async (_, res) => {
+router.get('/', auth, async (_, res) => {
   const users = await getUsers();
 
   res.status(200).json(users);
 });
 
-router.get('/todos', async (_, res) => {
+router.get('/todos', auth, async (_, res) => {
   const tables = await getTodos();
 
   res.status(200).json(tables);
